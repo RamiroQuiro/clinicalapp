ced25c9af6b014fa3a91398f0eebc53863f7d979
ramiroquiro <rama.exe.13@gmail.com>
2025-01-14 22:21:06 -0300

    craendo compontne de sala de espera

--- src/db/schema/listaDeEspera.ts
+++ src/db/schema/listaDeEspera.ts
@@ -1,11 +1,13 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const turnos=sqliteTable('turnos',{
+export const listaDeEspera=sqliteTable('listaDeEspera',{
     id:text('id').primaryKey().unique(),
     pacienteId:text('pacienteId').notNull(),
     userId:text('userId').notNull(),
     fecha:text('fecha').notNull(),
     hora:text('hora').notNull(),
+    orden:integer('orden',{mode:'number'}),
+    estado:text('estado').default('pendiente'),
     motivoConsulta:text('motivoConsulta').notNull(),
     activo: integer('activo',{mode:'boolean'}).default(true),
 })
---
+++ src/pages/dashboard/dashboard/componente/salaDeEspera.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function salaDeEspera() {
+  return (
+    <div>salaDeEspera</div>
+  )
+}
