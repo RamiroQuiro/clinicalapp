548bd27aa78771da05f86022f2b4c440db08e238
Ramiro Quiroga <ramiro.quiroga@cepsi.net.ar>
2025-01-29 15:34:54 -0300

    base de datos push

--- src/db/schema/doctoresPacientes.ts
+++ src/db/schema/doctoresPacientes.ts
@@ -1,9 +1,6 @@
-import { primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const doctoresPacientes = sqliteTable('doctoresPacientes', {
   userId: text('userId').notNull(), // ID del doctor
   pacienteId: text('pacienteId').notNull(), // ID del paciente en pacientesUnicos
-
-  // Clave única para evitar duplicados en la relación
-  uniqueConstraint: primaryKey('userId', 'pacienteId'),
 });
