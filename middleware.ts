168cad32903dd6587d4cb11c5e92d79d08416c9e
ramiroquiro <rama.exe.13@gmail.com>
2025-01-29 01:29:03 -0300

    corecion midleeware

--- middleware.ts
+++ middleware.ts
@@ -63,7 +63,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
       context.locals.user = null;
       context.locals.session = null;
     }
-    const sessionCookie = await lucia.validateSession(sessionId);
     const { session, user } = await lucia.validateSession(sessionId);
     if (session && session.fresh) {
       const sessionCookie = lucia.createSessionCookie(session.id);
